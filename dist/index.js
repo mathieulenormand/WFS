064cf83e10d79991d027d28d8a2d261b086bca6a
mathieu lenormand <mathieulenormand@MacBook-Pro-de-mathieu.local>
2018-06-22 10:58:06 +0200

    new forked all

--- dist/index.js
+++ dist/index.js
@@ -37,14 +37,25 @@ var Bar = exports.Bar = function Bar() {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+	value: true
 });
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Foo = exports.Foo = function Foo() {
-  _classCallCheck(this, Foo);
-};
+var Foo = exports.Foo = function () {
+	function Foo() {
+		_classCallCheck(this, Foo);
+	}
+
+	_createClass(Foo, [{
+		key: "hello",
+		value: function hello() {}
+	}]);
+
+	return Foo;
+}();
 
 },{}],3:[function(require,module,exports){
 'use strict';
--- src/Foo.js
+++ src/Foo.js
@@ -1,2 +1,5 @@
 export class Foo {
+	hello() {
+		
+	}
 }
